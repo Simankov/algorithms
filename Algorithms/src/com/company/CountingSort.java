8e04aa1db0cce543dec05d0cf53851e20c96cb20
Simankov <simankov14@gmail.com>
2015-08-29 18:25:41 +0300

    Counting sort with variable lengtht (8.3(a))

--- Algorithms/src/com/company/CountingSort.java
+++ Algorithms/src/com/company/CountingSort.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import org.w3c.dom.css.Counter;
+
 /**
  * Created by sergey on 29.08.15.
  */
 public class CountingSort extends Sorting{
    static int COUNTER_SIZE = 100000;
     int [] counter;
+    int [] B;
+    int [] C;
 
    static public void setCounterSize(int size) {
         COUNTER_SIZE = size;
@@ -32,9 +36,78 @@ public class CountingSort extends Sorting{
         }
     }
 
+    void CountingSortWithVariableLenght(){
+        // Using two arrays of size n and two arrays of size k
+        // Time complexify is O(n+k)
+
+
+        int start = 0;
+        int digitNumber = 0;
+        int p = A.length;
+        int [] sortedArray = new int[p];
+        int[] oneDigitCounter = new int[COUNTER_SIZE];
+        B = A.clone();
+        C = A.clone();
+
+        while (start < p-1) {
+            oneDigitCounter = new int[COUNTER_SIZE];
+            counter = new int[COUNTER_SIZE];
+
+            for (int i = start; i < p; i++) {
+                int digit = digit(i, digitNumber);
+                if (!isDigitBefore(i, digitNumber)) {
+                    oneDigitCounter[digit]++;
+                } else {
+                    counter[digit]++;
+                }
+            }
+
+            for (int i = 1; i < COUNTER_SIZE; i++) {
+                counter[i] = counter[i - 1] + counter[i];
+                oneDigitCounter[i] = oneDigitCounter[i - 1] + oneDigitCounter[i];
+            }
+            int offset = oneDigitCounter[COUNTER_SIZE - 1];
+
+            for (int j = p - 1; j >= start; j--) {
+                int digit = digit(j, digitNumber);
+                int oneDigitCount = oneDigitCounter[digit];
+                int count = counter[digit];
+                if (oneDigitCount > 0 && !isDigitBefore(j,digitNumber)) {
+                    sortedArray[start + oneDigitCount - 1] = B[j];
+                    oneDigitCounter[digit]--;
+                } else {
+                    if (count > 0) {
+                        C[start + count - 1 + offset] = B[j];
+                        counter[digit]--;
+                    }
+                }
+            }
+            B = C.clone();
+            start += offset;
+            digitNumber++;
+        }
+        A = sortedArray;
+
+    }
+
+    int digit(int i, int digitNumber){
+        String number = String.valueOf(B[i]);
+        char digit =  number.charAt(number.length() - (digitNumber+1));
+        return Character.getNumericValue(digit);
+    }
+
+    boolean isDigitBefore(int i, int digitNumber){
+        String number = String.valueOf(B[i]);
+        if (number.length()-(digitNumber+1)==0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     @Override
     void sort() {
-        CountingSort();
+        CountingSortWithVariableLenght();
     }
 
     void swap(int i, int j){
